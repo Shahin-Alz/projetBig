3f8e4d17e4c073c8d8d09748c43a1824203809b5
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-30 17:03:00 +0100

    ajout

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+let btnStarter = document.getElementById("btnSection5Starter");
+let btnDinner = document.getElementById("btnSection5Dinner");
+let btnLunch = document.getElementById("btnSection5Lunch");
+let btnBreakFast = document.getElementById("btnSection5Breakfast");
+
+btnStarter.addEventListener('click',function () {
+    btnStarter.classList.add("display:block")
+})
