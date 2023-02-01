bc5a13052d04cfabdcb1080dd510d9c4ce893248
Shahin-Alz <shaalemzadeh@gmail.com>
2023-02-01 11:28:33 +0100

    ajout de la section11

---
+++ public/js/module/_section10.js
@@ -0,0 +1,206 @@
+let btn1 = document.querySelector("#btn1section10");
+let btn2 = document.querySelector("#btn2section10");
+let btn3 = document.querySelector("#btn3section10");
+let btn4 = document.querySelector("#btn4section10");
+let btn5 = document.querySelector("#btn5section10");
+let btn6 = document.querySelector("#btn6section10");
+let btn7 = document.querySelector("#btn7section10");
+let btn8 = document.querySelector("#btn8section10");
+
+let img1 = document.querySelector("#img1section10");
+let img2 = document.querySelector("#img2section10");
+let img3 = document.querySelector("#img3section10");
+let img4 = document.querySelector("#img4section10");
+let img5 = document.querySelector("#img5section10");
+let img6 = document.querySelector("#img6section10");
+let img7 = document.querySelector("#img7section10");
+let img8 = document.querySelector("#img8section10");
+
+let myDivSection10 = document.querySelector(".allImgSection10");
+
+btn4.addEventListener("click", function () {
+    myDivSection10.style.setProperty("transform", "translateX(-26%)");
+    img1.classList.remove("imgSecSection10");
+    img1.classList.add("imgSection10");
+    img2.classList.remove("imgSecSection10");
+    img2.classList.add("imgSection10");
+    img3.classList.remove("imgSecSection10");
+    img3.classList.add("imgSection10");
+    
+    img5.classList.remove("imgSecSection10");
+    img5.classList.add("imgSection10");
+    img6.classList.remove("imgSecSection10");
+    img6.classList.add("imgSection10");
+    img7.classList.remove("imgSecSection10");
+    img7.classList.add("imgSection10");
+    img8.classList.remove("imgSecSection10");
+    img8.classList.add("imgSection10");
+
+
+    img4.classList.remove("imgSection10");
+    img4.classList.add("imgSecSection10");
+})
+
+btn5.addEventListener("click", function () {
+    myDivSection10.style.setProperty("transform", "translateX(-52%)");
+    img1.classList.remove("imgSecSection10");
+    img1.classList.add("imgSection10");
+    img2.classList.remove("imgSecSection10");
+    img2.classList.add("imgSection10");
+    img3.classList.remove("imgSecSection10");
+    img3.classList.add("imgSection10");
+    img6.classList.remove("imgSecSection10");
+    img6.classList.add("imgSection10");
+    img7.classList.remove("imgSecSection10");
+    img7.classList.add("imgSection10");
+    img8.classList.remove("imgSecSection10");
+    img8.classList.add("imgSection10");
+
+
+    img4.classList.remove("imgSecSection10");
+    img4.classList.add("imgSection10")
+    img5.classList.remove("imgSection10");
+    img5.classList.add("imgSecSection10");
+})
+
+btn6.addEventListener("click", function () {
+    myDivSection10.style.setProperty("transform", "translateX(-78%)");
+    img1.classList.remove("imgSecSection10");
+    img1.classList.add("imgSection10");
+    img2.classList.remove("imgSecSection10");
+    img2.classList.add("imgSection10");
+    img3.classList.remove("imgSecSection10");
+    img3.classList.add("imgSection10");
+    img4.classList.remove("imgSecSection10");
+    img4.classList.add("imgSection10");
+   
+    img7.classList.remove("imgSecSection10");
+    img7.classList.add("imgSection10");
+    img8.classList.remove("imgSecSection10");
+    img8.classList.add("imgSection10");
+
+
+
+
+    img5.classList.remove("imgSecSection10");
+    img5.classList.add("imgSection10")
+    img6.classList.remove("imgSection10");
+    img6.classList.add("imgSecSection10");
+})
+
+btn7.addEventListener("click", function () {
+    myDivSection10.style.setProperty("transform", "translateX(-104%)");
+    img1.classList.remove("imgSecSection10");
+    img1.classList.add("imgSection10");
+    img2.classList.remove("imgSecSection10");
+    img2.classList.add("imgSection10");
+    img3.classList.remove("imgSecSection10");
+    img3.classList.add("imgSection10");
+    img4.classList.remove("imgSecSection10");
+    img4.classList.add("imgSection10");
+    img5.classList.remove("imgSecSection10");
+    img5.classList.add("imgSection10");
+    
+    img8.classList.remove("imgSecSection10");
+    img8.classList.add("imgSection10");
+
+
+
+    img6.classList.remove("imgSecSection10");
+    img6.classList.add("imgSection10")
+    img7.classList.remove("imgSection10");
+    img7.classList.add("imgSecSection10");
+})
+
+btn8.addEventListener("click", function () {
+    myDivSection10.style.setProperty("transform", "translateX(-130%)");
+    img1.classList.remove("imgSecSection10");
+    img1.classList.add("imgSection10");
+    img2.classList.remove("imgSecSection10");
+    img2.classList.add("imgSection10");
+    img3.classList.remove("imgSecSection10");
+    img3.classList.add("imgSection10");
+    img4.classList.remove("imgSecSection10");
+    img4.classList.add("imgSection10");
+    img5.classList.remove("imgSecSection10");
+    img5.classList.add("imgSection10");
+    img6.classList.remove("imgSecSection10");
+    img6.classList.add("imgSection10");
+   
+
+    img7.classList.remove("imgSecSection10");
+    img7.classList.add("imgSection10")
+    img8.classList.remove("imgSection10");
+    img8.classList.add("imgSecSection10");
+})
+
+btn3.addEventListener("click", function () {
+    myDivSection10.style.setProperty("transform", "translateX(-5%)");
+    img1.classList.remove("imgSecSection10");
+    img1.classList.add("imgSection10");
+    img2.classList.remove("imgSecSection10");
+    img2.classList.add("imgSection10");
+    img3.classList.remove("imgSecSection10");
+    img3.classList.add("imgSection10");
+    img5.classList.remove("imgSecSection10");
+    img5.classList.add("imgSection10");
+    img6.classList.remove("imgSecSection10");
+    img6.classList.add("imgSection10");
+    img7.classList.remove("imgSecSection10");
+    img7.classList.add("imgSection10");
+    img8.classList.remove("imgSecSection10");
+    img8.classList.add("imgSection10");
+
+
+
+    img4.classList.remove("imgSecSection10");
+    img4.classList.add("imgSection10")
+    img3.classList.remove("imgSection10");
+    img3.classList.add("imgSecSection10");
+})
+
+btn2.addEventListener("click", function () {
+    myDivSection10.style.setProperty("transform", "translateX(22%)");
+    img1.classList.remove("imgSecSection10");
+    img1.classList.add("imgSection10");
+    img4.classList.remove("imgSecSection10");
+    img4.classList.add("imgSection10");
+    img5.classList.remove("imgSecSection10");
+    img5.classList.add("imgSection10");
+    img6.classList.remove("imgSecSection10");
+    img6.classList.add("imgSection10");
+    img7.classList.remove("imgSecSection10");
+    img7.classList.add("imgSection10");
+    img8.classList.remove("imgSecSection10");
+    img8.classList.add("imgSection10");
+
+
+
+
+    img3.classList.remove("imgSecSection10");
+    img3.classList.add("imgSection10");
+    img2.classList.remove("imgSection10");
+    img2.classList.add("imgSecSection10");
+})
+
+btn1.addEventListener("click", function () {
+    myDivSection10.style.setProperty("transform", "translateX(45%)");
+    img2.classList.remove("imgSecSection10");
+    img2.classList.add("imgSection10");
+    img3.classList.remove("imgSecSection10");
+    img3.classList.add("imgSection10");
+    img4.classList.remove("imgSecSection10");
+    img4.classList.add("imgSection10");
+    img5.classList.remove("imgSecSection10");
+    img5.classList.add("imgSection10");
+    img6.classList.remove("imgSecSection10");
+    img6.classList.add("imgSection10");
+    img7.classList.remove("imgSecSection10");
+    img7.classList.add("imgSection10");
+    img8.classList.remove("imgSecSection10");
+    img8.classList.add("imgSection10");
+
+
+    img1.classList.remove("imgSection10");
+    img1.classList.add("imgSecSection10");
+})
