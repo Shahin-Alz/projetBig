438d7e2d24361cc632e3a389111b8b8a75dc8fe2
Shahin-Alz <shaalemzadeh@gmail.com>
2023-02-03 16:12:20 +0100

    ajout

--- public/js/main.js
+++ public/js/main.js
@@ -337,3 +337,55 @@ const body = document.body;
 darkModeBtn.addEventListener("click", function () {
     body.style.backgroundColor = body.style.backgroundColor === "white" ? "black" : "white";
 });
+
+
+//////////////////////MODALE
+
+
+let modal = document.getElementById("myModal");
+
+
+let btn = document.querySelector(".BtnBookTableNavBar");
+
+
+let span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+//////////////////////////////////
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
